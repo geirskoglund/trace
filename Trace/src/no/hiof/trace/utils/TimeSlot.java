d49a8ae41fead136c232490f415f1e723836d309
Jon Are Wisting <jon.are.wisting@gmail.com>
2014-05-01 20:01:24 +0200

    kommentarer

--- Trace/src/no/hiof/trace/utils/TimeSlot.java
+++ Trace/src/no/hiof/trace/utils/TimeSlot.java
@@ -2,6 +2,10 @@ package no.hiof.trace.utils;
 
 import java.util.Date;
 
+/**
+ * @author  Trace Inc.
+ * A class for keeping time slots. Based on seconds to get hours, minutes and seconds elapsed.
+ */
 public class TimeSlot 
 {
 	final int MIN = 60;
@@ -14,6 +18,10 @@ public class TimeSlot
 	Date older; 
 	Date newer;
 		
+	/**
+	 * Constructor for TimeSlot
+	 * @param seconds the elapsed time of the time slot
+	 */
 	public TimeSlot(long seconds)
 	{
 		this.hours = (int) seconds/HOUR;
@@ -21,27 +29,47 @@ public class TimeSlot
 		this.seconds = (int) (seconds%HOUR) % MIN;
 	}
 	
+	/**
+	 * Example 5482 seconds = 1H, 31M, 22S
+	 * @return the amount of hours of the time slot
+	 */
 	public int getHours()
 	{
 		return hours;
 	}
 	
+	/**
+	 * Example 5482 seconds = 1H, 31M, 22S
+	 * @return the amount of minutes of the time slot.
+	 */
 	public int getMinutes()
 	{
 		return minutes;
 	}
 	
+	/**
+	 * Example 5482 seconds = 1H, 31M, 22S
+	 * @return the amount of seconds in the time slot
+	 */
 	public int getSeconds()
 	{
 		return seconds;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString()
 	{
 		return String.format("%s:%s:%s", leadingZero(hours), leadingZero(minutes), leadingZero(seconds));
 	}
 	
+	/**
+	 * Adds a leading zero to a number if it is lower than 10
+	 * @param number the number to add a leading zero to
+	 * @return the input number with or without a leading zero added
+	 */
 	private String leadingZero(int number)
 	{
 		if(number<10)
