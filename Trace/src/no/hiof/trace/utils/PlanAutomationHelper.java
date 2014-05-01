6a3b654abd2b69c82444ac2398e0e4f895b8ba20
Jon Are Wisting <jon.are.wisting@gmail.com>
2014-05-01 20:51:26 +0200

    kommentert

--- Trace/src/no/hiof/trace/utils/PlanAutomationHelper.java
+++ Trace/src/no/hiof/trace/utils/PlanAutomationHelper.java
@@ -9,6 +9,10 @@ import no.hiof.trace.db.model.Plan;
 import no.hiof.trace.db.values.ColumnName;
 import android.util.Log;
 
+/**
+ * @author Trace Inc.
+ *Helper class for automation of selection and registration
+ */
 public class PlanAutomationHelper {
 	
 	Plan ssidPlan = null;
@@ -16,10 +20,11 @@ public class PlanAutomationHelper {
 	String ssid = "";
 	String wifiTrigger="WiFi";
 
-	public PlanAutomationHelper() 
-	{
-	}
-	
+	/**
+	 * Gets the first plan in a collection with a registered SSID
+	 * @param ssid the SSID to check for
+	 * @return the first Plan with correct SSID
+	 */
 	public Plan getPlanForSSID(String ssid)
 	{	
 		if(!ssidIsQueried(ssid))
@@ -28,6 +33,11 @@ public class PlanAutomationHelper {
 		return getFirstPlanFrom(ssidPlans);
 	}
 	
+	/**
+	 * Gets all the plans that are registered with a given SSID
+	 * @param ssid to check for
+	 * @return a collection of Plans with the registered SSID
+	 */
 	public List<Plan> getPlansForSSID(String ssid)
 	{
 		if(!ssidIsQueried(ssid))
@@ -39,6 +49,11 @@ public class PlanAutomationHelper {
 		return ssidPlans;
 	}
 	
+	/**
+	 * Checks if a SSID is registered to a plan for auto loading. True if it is registered.
+	 * @param ssid the SSID to check
+	 * @return the result of the SSID check
+	 */
 	public boolean autoLoadingSetForSSID(String ssid)
 	{
 		if(!ssidIsQueried(ssid))
@@ -47,6 +62,10 @@ public class PlanAutomationHelper {
 		return !ssidPlans.isEmpty();
 	}
 	
+	/*Gets the first Plan-object from a collection of Plan-objects or an empty plan if the collection is empty
+	 * @param plans the collection of plans
+	 * @return the first Plan in the input collection
+	 */
 	private Plan getFirstPlanFrom(List<Plan> plans)
 	{
 		if(plans.isEmpty())
@@ -55,12 +74,21 @@ public class PlanAutomationHelper {
 			return plans.get(0);
 	}
 	
+	/**
+	 * Gets the current plan
+	 * @return the current Plan
+	 */
 	public Plan getCurrentPlan()
 	{
 		DatabaseManager database = new DatabaseManager(TraceApp.getAppContext());
 		return database.getActivePlan();
 	}
 	
+	/*
+	 * Used to check if the ssid field is equal to another SSID
+	 * @param ssid the SSID used to check against the 
+	 * @return the result of the SSID check
+	 */
 	private boolean ssidIsQueried(String ssid)
 	{
 		return this.ssid.equals(ssid);
